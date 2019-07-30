af9e3c779e2aeca44747a4a2c913838fd7899ecd
Muhanad Atef <muhanad.atef23@gmail.com>
2019-07-30 13:08:48 +0200

    edit & delete questions buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddQuestionComponent } from './add-question/add-question.component';
 import { LoginComponent } from './login/login.component';
+import { PreviewComponent } from './preview/preview.component';
 
 const routes: Routes = [
   {path: '',component: LoginComponent},
-  {path: 'AddQuestion', component: AddQuestionComponent}
+  {path: 'AddQuestion', component: AddQuestionComponent},
+  {path: 'previewExam', component: PreviewComponent}
 ];
 
 @NgModule({
