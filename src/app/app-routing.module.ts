f59aa2536f501eeff61dd6418e238d82e65e91f0
Muhanad Atef <muhanad.atef23@gmail.com>
2019-08-26 10:20:20 +0200

    edit,delete question

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddQuestionComponent } from './add-question/add-question.component';
 import { LoginComponent } from './login/login.component';
 import { PreviewComponent } from './preview/preview.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  {path: '',component: LoginComponent},
+  {path: '', component: LoginComponent},
   {path: 'AddQuestion', component: AddQuestionComponent},
-  {path: 'previewExam', component: PreviewComponent}
+  {path: 'previewExam/:username/:id', component: PreviewComponent},
+  {path: 'profile/:username', component: ProfileComponent}
 ];
 
 @NgModule({
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  uploadedImg = null;
+  pp: string = '../../assets/img/grey.png';
+  reader: FileReader = new FileReader();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  processImage(event) {
+    this.uploadedImg = event.target.files[0];
+    this.reader.onload = (event: any) => {
+      this.pp = event.target.result;
+      console.log(event.loaded / event.total * 100 + '%');
+    };
+    this.reader.readAsDataURL(this.uploadedImg);
   }
 
 }
