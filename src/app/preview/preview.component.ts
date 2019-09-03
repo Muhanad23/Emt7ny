334b5bb5172193f671691d06af95a067d3ed5483
Muhanad Atef <muhanad.atef23@gmail.com>
2019-09-03 05:38:02 +0200

    mcq

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreviewComponent implements OnInit {
 
+  fname = 'asd';
+  lname = 'otta';
+  f:number[] = [0, 0];
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit() {
+    console.log("first = "+this.f[0]);
+    console.log("second = "+this.f[1]);
+  }
 }
+
