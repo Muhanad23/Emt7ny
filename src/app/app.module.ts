a657787fd34c03a36e24823dcb2d518090ed9736
Muhanad Atef <muhanad.atef23@gmail.com>
2019-09-04 06:16:48 +0200

    drawing area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'; 
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AddQuestionComponent } from './add-question/add-question.component';
 import { PreviewComponent } from './preview/preview.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SignaturePadModule } from 'angular2-signaturepad';
+
 
 @NgModule({
   declarations: [
@@ -14,12 +16,13 @@ import { ProfileComponent } from './profile/profile.component';
     LoginComponent,
     AddQuestionComponent,
     PreviewComponent,
-    ProfileComponent
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SignaturePadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
